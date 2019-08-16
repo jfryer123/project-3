24753286f8f8380be15b997f48c2a25ae98b4cf6
Brooke Kennison <brookelkennison@gmail.com>
2019-08-16 11:40:39 -0500

    merge

--- controllers/sessions.js
+++ controllers/sessions.js
@@ -6,37 +6,38 @@ require('dotenv').config();
 
 const bcrypt = require('bcrypt');
 
+router.delete('/', (req, res)=>{
+    //destroy session
+    req.session.destroy(() => {
+        //redirect back to welcome page
+        res.status(200).json({
+          status:200,
+          message:'logout complete'
+        });
+    })
+});
+
 router.post('/', (req, res)=>{
-    console.log("sessions/post");
+    //find user based on the username that they typed in /sessions/new
     User.findOne({username:req.body.username}, (err, foundUser)=>{
+        //compare what the user typed for password to the encrypted db value
         if(bcrypt.compareSync(req.body.password, foundUser.password)){
             req.session.currentUser = foundUser;
-            res.json(foundUser);
+            res.status(201).json({
+              status:201,
+              message:'session created'
+            });
         } else {
-            res.json({
-                status:401
+            //if the passwords don't match, tell the user
+            // res.send('wrong password');
+            res.status(401).json({
+              status:401,
+              message:'login failed'
             });
         }
-    });
-});
+    })
+})
 
-<<<<<<< HEAD
-router.delete('/', (req, res)=>{
-    req.session.destroy(() => {
-		res.json({
-			status:200
-		})
-    });
-});
-
-router.post('/newuser', (req, res)=>{
-	console.log(req.body.username);
-    console.log(req.body.password);
-    req.body.password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10));
-	console.log(req.body.password);
-    User.create(req.body, (err, createdUser)=>{
-        res.json(createdUser);
-=======
 router.post('/newuser', (req, res)=>{
   console.log("3" + req.body.password);
     //encrypt what the user typed for password
@@ -48,12 +49,8 @@ router.post('/newuser', (req, res)=>{
           status:202,
           message:"user created"
         });
->>>>>>> 8be4355012c5ff513e612ce389d4b5886b4884d1
     });
 });
 
-router.delete('/user/:id', (req, res)=>{
-    User.findOneAndRemove();
-});
 
 module.exports = router;
